f6f2e47c78263a648f775bfe9a4bd3de8da0ded0
ical10 <mochhusnir@gmail.com>
2022-10-01 17:01:25 +0700

    chore: trial using dotenv

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,60 @@
 import { HardhatUserConfig, task } from "hardhat/config";
+import type { NetworkUserConfig } from "hardhat/types";
 import "@nomicfoundation/hardhat-toolbox";
 
+import * as dotenv from "dotenv";
+import { resolve } from "path";
+
+const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || "./.env";
+dotenv.config({ path: resolve(__dirname, dotenvConfigPath) });
+
+// Ensure that we have all the environment variables we need.
+const mnemonic: string | undefined = process.env.MNEMONIC;
+if (!mnemonic) {
+  throw new Error("Please set your MNEMONIC in a .env file");
+}
+
+const infuraApiKey: string | undefined = process.env.INFURA_API_KEY;
+if (!infuraApiKey) {
+  throw new Error("Please set your INFURA_API_KEY in a .env file");
+}
+
+const chainIds = {
+  goerli: 5,
+  hardhat: 31337,
+  mainnet: 1,
+};
+
+function getChainConfig(chain: keyof typeof chainIds): NetworkUserConfig {
+  let jsonRpcUrl: string;
+  switch (chain) {
+    default:
+      jsonRpcUrl = "https://" + chain + ".infura.io/v3/" + infuraApiKey;
+  }
+  return {
+    accounts: {
+      count: 10,
+      mnemonic,
+      path: "m/44'/60'/0'/0",
+    },
+    chainId: chainIds[chain],
+    url: jsonRpcUrl,
+  };
+}
+
 const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic,
+      },
+      chainId: chainIds.hardhat,
+    },
+    goerli: getChainConfig("goerli"),
+  },
   solidity: "0.8.17",
-  paths: { tests: "tests" },
+  paths: { tests: "./tests" },
 };
 
 //hardhat.org/hardhat-runner/docs/guides/tasks-and-scripts#writing-hardhat-tasks
